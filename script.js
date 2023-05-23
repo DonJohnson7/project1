0d570b62345824ce290d5584783c760a7bd7cb99
DonJohnson7 <134338366+DonJohnson7@users.noreply.github.com>
2023-05-23 13:34:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-document.getElementById('flipButton').addEventListener('click', function() {
-    // Randomly choose between head and tail
-    let result = Math.random() < 0.5 ? 'head.png' : 'tail.png';
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Coin Flip Game</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <h1>Coin Flip Game</h1>
+    <img id="coin" src="head.png" alt="coin">
 
-    // Change the coin image
-    document.getElementById('coin').src = result;
+    <h2>Your money: $<span id="money">100</span></h2>
+    <label for="bet">Bet Amount: </label>
+    <input id="bet" type="number" min="1" max="100" value="10">
+    <label for="choice">Your choice: </label>
+    <select id="choice">
+        <option value="head.png">Heads</option>
+        <option value="tail.png">Tails</option>
+    </select>
+    <button id="flipButton">Flip the Coin!</button>
 
-    // Print the result
-    console.log('The result is: ' + (result === 'head.png' ? 'Head' : 'Tail'));
-});
+    <script src="script.js"></script>
+</body>
+</html>
