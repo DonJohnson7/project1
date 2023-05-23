133423bbb96f1dfbcf5f3dd91d9519e246380f48
DonJohnson7 <134338366+DonJohnson7@users.noreply.github.com>
2023-05-23 12:30:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.getElementById('flipButton').addEventListener('click', function() {
     // Randomly choose between head and tail
-    let result = Math.random() < 0.5 ? 'head.png' : 'tail.png';
+    let result = Math.random() < 0.5 ? 'C:\Users\Lenovo\Documents\GitHub\project1\heads.png' : 'C:\Users\Lenovo\Documents\GitHub\project1\tails.png';
 
     // Change the coin image
     document.getElementById('coin').src = result;
 
     // Print the result
-    console.log('The result is: ' + (result === 'head.png' ? 'Head' : 'Tail'));
+    console.log('The result is: ' + (result === 'C:\Users\Lenovo\Documents\GitHub\project1\heads.png' ? 'Head' : 'Tail'));
 });
